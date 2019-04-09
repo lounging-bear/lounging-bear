03a4df1f7153f23844573e1c0e548ccae51a9a1f
Johannes Klumpe <johannes.klumpe@gmail.com>
2019-04-09 13:57:43 +0200

    Adds open sans as typeface

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,7 @@
  */
 const GoogleAnalytics = require('react-ga')
 const config = require('./config/meta')
+require('typeface-open-sans')
 
 /**
  * Initialize Google Analytics
--- src/util/style/GlobalStyles.js
+++ src/util/style/GlobalStyles.js
@@ -5,15 +5,6 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  @font-face {
-    font-family: 'AzoSansRegular';
-    src:  url('./fonts/AzoSans-Regular.ttf'),
-         url('./fonts/azosans-regular-webfont.woff2') format('woff2'),
-         url('./fonts/azosans-regular-webfont.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
-
   html,
   body {
     min-height: 100%;
